2a06edb219e9634b06c6c49bb4d0fb21e66b1ae7
rcalves82 <rclarindo.alves@gmail.com>
2023-02-07 03:52:53 -0300

    ajustando e finalizando o teste de back

--- back-end/features/spec/validar-api-vrpat.feature
+++ back-end/features/spec/validar-api-vrpat.feature
@@ -1,14 +1,15 @@
 # language: pt
 
+@get
 Funcionalidade: Consultar informações API dos produtos VR
 Como um usuário do sistema
 Eu quero realizar requisiçoes de consulta na API
 A fim de verificar quais produtos e estabelecimentos retornam no serviço
 
-Esquema do Cenário: Consultar informações estabelecimento na API
-    Dado o endereço da API de produtos da VR
-    Então a API irá retornar informações referente ao estabelecimento "<local>"
-
+Esquema do Cenário: Consultar informações de estabelecimento na API
+    Dado o endereço da API de produtos e estabelecimento da VR
+    Quando informo um estabelecimento aleatorio
+    Então a API irá retornar o estabelecimento "<local>" informado
     Exemplos: 
         |local|
         |PIZZARIA|
--- back-end/features/step_definitions/validar-api-vrpat.rb
+++ back-end/features/step_definitions/validar-api-vrpat.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
-Dado('o endereço da API de produtos da VR') do
+Dado('o endereço da API de produtos e estabelecimento da VR') do
     @get_produtos = GetService.get('/VRPAT')
 end
 
-Então('a API irá retornar informações referente ao estabelecimento {string}') do |local|
+Quando('informo um estabelecimento aleatorio') do
     # criando uma variavel que recebe o array typeOfEstablishment na posição ZERO
-    nome_local = @get_produtos["typeOfEstablishment"][0]
+    @nome_local = @get_produtos["typeOfEstablishment"][0]
+end
+
+Então('a API irá retornar o estabelecimento {string} informado') do |local|
     expect(@get_produtos.code).to eq 200
     expect(@get_produtos.message).to eq "OK"
     # aqui verifico se o estabelecimento existe
-    expect(nome_local["name"]).to eq local
+    expect(@nome_local["name"]).to eq local
 end
