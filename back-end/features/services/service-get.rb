90b6da73a2cc59f9b2589c6ece487867f24aa7ad
rcalves82 <rclarindo.alves@gmail.com>
2023-02-07 04:16:59 -0300

    novamente ajustando e finalizando o teste de back

--- back-end/features/services/service-get.rb
+++ back-end/features/services/service-get.rb
@@ -3,8 +3,4 @@ module GetService
     include HTTParty
     #url base da API
     base_uri 'https://portal.vr.com.br/api-web/comum/enumerations'
-    format :json
-    open_timeout 30
-    follow_redirects false
-    headers 'Content-Type' => 'application/json'
 end
